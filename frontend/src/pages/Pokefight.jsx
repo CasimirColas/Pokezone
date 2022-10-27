5430aac9eaa27f4dadebf7dab7e04c3415f8f087
casimir colas <casimir.colas@gmail.com>
2022-10-27 17:53:40 +0200

    random pokemon is registered in useState

--- frontend/src/pages/Pokefight.jsx
+++ frontend/src/pages/Pokefight.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Attack from "../components/Pokefight/Attack";
 import FightingPoke from "../components/Pokefight/FightingPoke";
 import pokeVS from "../assets/pokeVS.png";
 
 function Pokefight() {
-  const names = [];
-  React.useEffect(() => {
+  const [names, setNames] = useState();
+  function randomTab(tab) {
+    if (!tab) {
+      return tab;
+    }
+    const nb = Math.floor(Math.random() * tab.length);
+    return tab[nb];
+  }
+  useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
       .then((response) => response.data)
       .then((data) => {
         const u = data.results;
+        const result = [];
         u.forEach((e) => {
-          names.push(e.name);
+          result.push(e.name);
         });
+        setNames(result);
       });
   }, []);
   return (
     <div className="Pokefight">
       <header>Pokeheader</header>
       <div className="versusbar">
+        <h1>{randomTab(names)}</h1>
         <h3>Name1</h3>
         <img src={pokeVS} alt="not found" />
         <h3>Name2</h3>
