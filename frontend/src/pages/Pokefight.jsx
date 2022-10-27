9838f05dea49cc20aee45032844c19ef5ce7a8b8
CasimirColas <casimir.colas@gmail.com>
2022-10-27 22:54:33 +0200

    added creation of random pokemon

--- frontend/src/pages/Pokefight.jsx
+++ frontend/src/pages/Pokefight.jsx
@@ -6,12 +6,25 @@ import pokeVS from "../assets/pokeVS.png";
 
 function Pokefight() {
   const [names, setNames] = useState();
+  const [myPoke, setMyPoke] = useState();
+  const [enmyPoke, setEnmyPoke] = useState();
   function randomTab(tab) {
     if (!tab) {
       return tab;
     }
     const nb = Math.floor(Math.random() * tab.length);
     return tab[nb];
+  }
+  function createPokemon(nameP) {
+    const pokemon = {
+      name: nameP,
+      ability: "test",
+    };
+    return pokemon;
+  }
+  function randomSet() {
+    setMyPoke(createPokemon(randomTab(names)));
+    setEnmyPoke(createPokemon(randomTab(names)));
   }
   useEffect(() => {
     axios
@@ -24,16 +37,23 @@ function Pokefight() {
           result.push(e.name);
         });
         setNames(result);
+        setMyPoke(createPokemon(randomTab(result)));
+        setEnmyPoke(createPokemon(randomTab(result)));
       });
   }, []);
+  // setMyPoke(createPokemon(randomTab(names)))
+  // setEnmyPoke(createPokemon(randomTab(names)))
+  console.warn(myPoke, enmyPoke);
   return (
     <div className="Pokefight">
       <header>Pokeheader</header>
+      <button type="submit" onClick={randomSet}>
+        New set
+      </button>
       <div className="versusbar">
-        <h1>{randomTab(names)}</h1>
-        <h3>Name1</h3>
+        <h3>myPoke</h3>
         <img src={pokeVS} alt="not found" />
-        <h3>Name2</h3>
+        <h3>enmyPoke</h3>
       </div>
       <div className="terrain">
         <FightingPoke player={0} info="pokE" />
