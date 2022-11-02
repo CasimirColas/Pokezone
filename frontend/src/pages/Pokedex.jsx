388db3d85ab887dd188c19f685e28d04b126a0ec
Scottlaine <alfian.scottlaine@gmail.com>
2022-11-02 11:07:58 +0100

    add pokeball in pokedex

--- frontend/src/pages/Pokedex.jsx
+++ frontend/src/pages/Pokedex.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import Pokemon2 from "../assets/images/Pokemon2.png";
-import "../components/css/home.css";
+import "../components/css/pokedex.css";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import "animate.css";
 
 function PokeDex() {
   return (
-    <div className="pokemon">
-      <img src={Pokemon2} width="300px" height="200px" alt="pokemon" />
-    </div>
+    <body>
+      <div className="animate__animated animate__backInUp">
+        <article className="pokeup" />
+      </div>
+      <div className="animate__animated animate__backInDown">
+        <article className="pokedown" />
+      </div>
+      <div>
+        <article className="smallBall" />
+      </div>
+    </body>
   );
 }
 
