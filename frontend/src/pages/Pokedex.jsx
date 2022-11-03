aa86cf58555b13cf5425d1e37a894431de3c8344
Scottlaine <alfian.scottlaine@gmail.com>
2022-11-03 10:28:11 +0100

    add pokeball animation etc

--- frontend/src/pages/Pokedex.jsx
+++ frontend/src/pages/Pokedex.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "../components/css/pokedex.css";
+import PokeDown from "../assets/images/PokeDown.png";
+import PokeUp from "../assets/images/PokeUp.png";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import "animate.css";
+// import "animate.css";
 
 function PokeDex() {
   return (
     <body>
-      <div className="animate__animated animate__backInUp">
-        <article className="pokeup" />
+      <div className="top_pokeball_parent">
+        <img
+          src={PokeUp}
+          className="pokeup animate__animated animate__slideInUp animate__delay-0.5s"
+          alt="pokeup"
+        />
       </div>
-      <div className="animate__animated animate__backInDown">
-        <article className="pokedown" />
+      <div className="bottom_pokeball_parent">
+        <img
+          src={PokeDown}
+          className="pokedown animate__animated animate__slideInDown animate__delay-0.5s"
+          alt="pokedown"
+        />
       </div>
-      <div>
-        <article className="smallBall" />
+      <div className="poke_list_parent">
+        <article />
       </div>
     </body>
   );
