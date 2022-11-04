1d76011ad79319bf4cc91350de1f76e0d4a6d89b
Casimir Colas <casimir.colas@gmail.com>
2022-11-04 14:54:28 +0100

    added a new page with only async functions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
-import Pokefight from "./pages/Pokefight";
+// import Pokefight from "./pages/Pokefight";
+import PokefightALT from "./pages/PokefightALT";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Pokefight />
+    <PokefightALT />
   </React.StrictMode>
 );
