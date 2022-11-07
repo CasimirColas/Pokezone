42c74fee74192303ac56c5f0b32d051e00092b00
Casimir Colas <casimir.colas@gmail.com>
2022-11-07 10:04:03 +0100

    cleaning branch footer adjusted

--- frontend/src/pages/Pokefight.jsx
+++ frontend/src/pages/Pokefight.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Attack from "../components/Pokefight/Attack";
 import FightingPoke from "../components/Pokefight/FightingPoke";
 import pokeVS from "../assets/pokeVS.png";
+import "./css/pokefight.css";
 
 function Pokefight() {
   const [names, setNames] = useState();
@@ -15,93 +16,94 @@ function Pokefight() {
     const nb = Math.floor(Math.random() * tab.length);
     return tab[nb];
   }
-  function isDamage(nameM) {
-    return axios
-      .get(`https://pokeapi.co/api/v2/move/${nameM}`)
-      .then((response) => response.data)
-      .then((data) => {
-        const moveData = data;
-        if (
-          moveData.damage_class.name === "physical" ||
-          moveData.damage_class.name === "special"
-        ) {
-          return nameM;
-        }
-        return null;
-      });
+  async function getListOfNames() {
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon/?limit=151"
+    );
+    const pokedata = response.data.results;
+    const list = [];
+    pokedata.forEach((e) => {
+      list.push(e.name);
+    });
+    return list;
   }
-  // function getRandomAttack(nameM) {
-  //   return axios
-  //     .get(`https://pokeapi.co/api/v2/move/${nameM}`)
-  //     .then((response) => response.data)
-  //     .then((data) => {
-  //       const moveData = data;
-  //       const attack = {
-  //         accuracy: moveData.accuracy,
-  //         power: moveData.power,
-  //         type: moveData.type.name,
-  //       };
-  //       return attack;
-  //     });
-  // }
-  function createPoke(nameP) {
-    return axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${nameP}`)
-      .then((r) => r.data)
-      .then((data) => {
-        const pokedata = data;
-        const moveList = [];
-        pokedata.moves.forEach((e) => {
-          isDamage(e.move.name).then((r) => {
-            if (r) {
-              moveList.push(r);
-            }
-          });
-        });
-        const pokemon = {
-          name: nameP,
-          image: pokedata.sprites.other["official-artwork"].front_default,
-          hp: pokedata.stats[0].base_stat,
-          a: pokedata.stats[1].base_stat,
-          as: pokedata.stats[3].base_stat,
-          d: pokedata.stats[2].base_stat,
-          ds: pokedata.stats[4].base_stat,
-          speed: pokedata.stats[5].base_stat,
-          types: pokedata.types.map((e) => e.type.name),
-          color: "",
-          attack1: "1",
-          attack2: "2",
-          attack3: "3",
-          attack4: "4",
-        };
-        return pokemon;
-      });
+  async function isDmg(nameM) {
+    const result = await axios.get(`https://pokeapi.co/api/v2/move/${nameM}`);
+    const moveData = result.data;
+    if (moveData.power != null) {
+      return true;
+    }
+    return false;
   }
-  function randomSet() {
-    createPoke(randomTab(names)).then((r) => setMyPoke(r));
-    createPoke(randomTab(names)).then((r) => setEnmyPoke(r));
+  async function getAttackList(list) {
+    const result = [];
+    for await (const i of list) {
+      const test = await isDmg(i.move.name);
+      if (test) {
+        result.push(i.move.name);
+      }
+    }
+
+    return result;
+  }
+  async function getAttack(nameM) {
+    const response = await axios.get(`https://pokeapi.co/api/v2/move/${nameM}`);
+    const moveData = response.data;
+    const move = {
+      name: nameM,
+      accuracy: moveData.accuracy,
+      power: moveData.power,
+      type: moveData.type.name,
+    };
+    return move;
+  }
+  async function createPoke(nameP, data, aList) {
+    const pokemon = {
+      name: nameP,
+      image: data.sprites.other["official-artwork"].front_default,
+      hp: data.stats[0].base_stat,
+      a: data.stats[1].base_stat,
+      as: data.stats[3].base_stat,
+      d: data.stats[2].base_stat,
+      ds: data.stats[4].base_stat,
+      speed: data.stats[5].base_stat,
+      types: data.types.map((e) => e.type.name),
+      color: "",
+      attack1: await getAttack(randomTab(aList)),
+      attack2: await getAttack(randomTab(aList)),
+      attack3: await getAttack(randomTab(aList)),
+      attack4: await getAttack(randomTab(aList)),
+    };
+    return pokemon;
+  }
+  async function initiatePoke(nameP) {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${nameP}`
+    );
+    const pokedata = response.data;
+    const moveList = await getAttackList(pokedata.moves);
+    const pokemon = await createPoke(nameP, pokedata, moveList);
+    return pokemon;
+  }
+  async function randomSet() {
+    setMyPoke(await initiatePoke(randomTab(names)));
+    setEnmyPoke(await initiatePoke(randomTab(names)));
   }
   useEffect(() => {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-      .then((r) => r.data)
-      .then((data) => {
-        const u = data.results;
-        const result = [];
-        u.forEach((e) => {
-          result.push(e.name);
-        });
-        setNames(result);
-        createPoke(randomTab(result)).then((r) => setMyPoke(r));
-        createPoke(randomTab(result)).then((r) => setEnmyPoke(r));
-      });
+    const fetchData = async () => {
+      const genList = await getListOfNames();
+      setNames(genList);
+      setMyPoke(await initiatePoke(randomTab(genList)));
+      setEnmyPoke(await initiatePoke(randomTab(genList)));
+    };
+
+    fetchData().catch(console.error);
   }, []);
   if (!myPoke || !enmyPoke) {
     return <>loading...</>;
   }
   return (
     <div className="Pokefight">
-      <header>Pokeheader</header>
       <button type="submit" onClick={randomSet}>
         New set
       </button>
@@ -120,7 +122,6 @@ function Pokefight() {
         <Attack pos={3} info={myPoke.attack3} />
         <Attack pos={4} info={myPoke.attack4} />
       </div>
-      <footer>Pokefooter</footer>
     </div>
   );
 }
