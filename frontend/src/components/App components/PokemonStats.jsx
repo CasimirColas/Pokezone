84c87731219e5592ca6423467d916bfdacaccdfc
CasimirColas <casimir.colas@gmail.com>
2022-11-22 18:54:07 +0100

    stats bar lenght are dinamic

--- frontend/src/components/App components/PokemonStats.jsx
+++ frontend/src/components/App components/PokemonStats.jsx
@@ -19,7 +19,32 @@ function PokemonStats({ nom, onClick }) {
   if (!pokemon) {
     return <div>Loading...</div>;
   }
-
+  function barDisplay(s1, s2, s3, s4, s5, s6, n) {
+    let width = 0;
+    if (s1 > width) {
+      width = s1;
+    }
+    if (s2 > width) {
+      width = s2;
+    }
+    if (s3 > width) {
+      width = s3;
+    }
+    if (s4 > width) {
+      width = s4;
+    }
+    if (s5 > width) {
+      width = s5;
+    }
+    if (s6 > width) {
+      width = s6;
+    }
+    width = Math.floor((n / width) * 100);
+    return {
+      width: `${width}%`,
+      marginRight: `${100 - width}%`,
+    };
+  }
   const capitalizeFirst = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
@@ -90,40 +115,116 @@ function PokemonStats({ nom, onClick }) {
           <div className="stats-details">
             <h4>HP : </h4>
             <div className="color-container1">
-              <p>{pokemon.stats[0].base_stat}</p>
+              <p
+                style={barDisplay(
+                  pokemon.stats[0].base_stat,
+                  pokemon.stats[1].base_stat,
+                  pokemon.stats[2].base_stat,
+                  pokemon.stats[3].base_stat,
+                  pokemon.stats[4].base_stat,
+                  pokemon.stats[5].base_stat,
+                  pokemon.stats[0].base_stat
+                )}
+              >
+                {pokemon.stats[0].base_stat}
+              </p>
             </div>
           </div>
           <div className="stats-details">
             <h4>Attack : </h4>
             <div className="color-container2">
-              <p> {pokemon.stats[1].base_stat}</p>
+              <p
+                style={barDisplay(
+                  pokemon.stats[0].base_stat,
+                  pokemon.stats[1].base_stat,
+                  pokemon.stats[2].base_stat,
+                  pokemon.stats[3].base_stat,
+                  pokemon.stats[4].base_stat,
+                  pokemon.stats[5].base_stat,
+                  pokemon.stats[1].base_stat
+                )}
+              >
+                {" "}
+                {pokemon.stats[1].base_stat}
+              </p>
             </div>
           </div>
 
           <div className="stats-details">
             <h4>Defense : </h4>
             <div className="color-container3">
-              <p> {pokemon.stats[2].base_stat}</p>
+              <p
+                style={barDisplay(
+                  pokemon.stats[0].base_stat,
+                  pokemon.stats[1].base_stat,
+                  pokemon.stats[2].base_stat,
+                  pokemon.stats[3].base_stat,
+                  pokemon.stats[4].base_stat,
+                  pokemon.stats[5].base_stat,
+                  pokemon.stats[2].base_stat
+                )}
+              >
+                {" "}
+                {pokemon.stats[2].base_stat}
+              </p>
             </div>
           </div>
           <div className="stats-details">
             <h4>Sp-Attack : </h4>
             <div className="color-container4">
-              <p> {pokemon.stats[3].base_stat}</p>
+              <p
+                style={barDisplay(
+                  pokemon.stats[0].base_stat,
+                  pokemon.stats[1].base_stat,
+                  pokemon.stats[2].base_stat,
+                  pokemon.stats[3].base_stat,
+                  pokemon.stats[4].base_stat,
+                  pokemon.stats[5].base_stat,
+                  pokemon.stats[3].base_stat
+                )}
+              >
+                {" "}
+                {pokemon.stats[3].base_stat}
+              </p>
             </div>
           </div>
 
           <div className="stats-details">
             <h4>Sp-Defense : </h4>
             <div className="color-container5">
-              <p> {pokemon.stats[4].base_stat}</p>
+              <p
+                style={barDisplay(
+                  pokemon.stats[0].base_stat,
+                  pokemon.stats[1].base_stat,
+                  pokemon.stats[2].base_stat,
+                  pokemon.stats[3].base_stat,
+                  pokemon.stats[4].base_stat,
+                  pokemon.stats[5].base_stat,
+                  pokemon.stats[4].base_stat
+                )}
+              >
+                {" "}
+                {pokemon.stats[4].base_stat}
+              </p>
             </div>
           </div>
 
           <div className="stats-details">
             <h4>Speed : </h4>
             <div className="color-container6">
-              <p>{pokemon.stats[5].base_stat}</p>
+              <p
+                style={barDisplay(
+                  pokemon.stats[0].base_stat,
+                  pokemon.stats[1].base_stat,
+                  pokemon.stats[2].base_stat,
+                  pokemon.stats[3].base_stat,
+                  pokemon.stats[4].base_stat,
+                  pokemon.stats[5].base_stat,
+                  pokemon.stats[5].base_stat
+                )}
+              >
+                {pokemon.stats[5].base_stat}
+              </p>
             </div>
           </div>
         </div>
